e694e710330646e0ebc1cf45a3f27df23dde2be2
noneloik <loik.nata@gmail.com>
2018-12-14 17:20:28 +0200

    commit 4

--- src/components/GameCanvas.vue
+++ src/components/GameCanvas.vue
@@ -1,85 +1,88 @@
 <template>
   <div class="alert alert-secondary greeting">
-    <h1 class="pb-20">Please solve the below equation</h1>
+    <h4 class="pb-20">Please solve the below equation</h4>
     <h2> {{ x }} + {{ y }} = {{ result }} </h2>
-    <button v-for="(item, index) in answerOptions" :key="index" class="btn btn-primary answers" @click="insertOption">
-      {{ item }}
-      </button>
+    <button
+      v-for="(item, index) in answerOptions"
+      :key="index"
+      class="btn btn-primary answers"
+      @click="insertOption"
+    >
+    {{ item }}
+    </button>
     <hr>
     <button @click="proceed">Continue</button>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'GameCanvas',
-    data() {
-      return {
-        x: 0,
-        y: 0,
-        result: '?',
-        from: -100,
-        to: 100,
-        expectedResult: 0,
-        numberOfButtons: 3,
-        answerOptions: [],
-    //     intMsg:'',
-    //    currentStep:0,
-    //  numberOfSteps:3
-      }
-    },
-    methods: {
-      generateNumber(from, to) {
-        return Math.ceil(Math.random() * (to - from) + from)
-      },
-      generateMembers() {
-        this.x = this.generateNumber(this.from, this.to);
-        this.y = this.generateNumber(this.from, this.to);
-        this.expectedResult = this.x + this.y
-        this.answerOptions.push(this.expectedResult)
-        for (let i = 0; i < this.numberOfButtons - 1; i++) {
-          const newOption = this.expectedResult + this.generateNumber(-10, 10)
-          this.shuffle(this.answerOptions)
-        }
-      },
-      shuffle(targetArray) {
-        return targetArray.sort(() => {
-          return Math.random() - 0.5
-        })
-      },
-      insertOption(event) {
-        this.result = event.target.innerHTML
-      },
-      proceed() {
-        if (this.result === '?') {
-          alert('Please select the answer');
-          return;
-        }
-        if (this.result == this.expectedResult) {
-          this.$emit('checkOneAnswer', `Your answer is correct !
-          Would you like to proceed`)
-        } else {
-          this.$emit('checkOneAnswer', `Your answer is wrong,
-          Correct answer is ${this.expectedResult}!
-          Would you like to proceed`)
-        }
-        this.$emit('switchView', 'Info')
-      },
-      created() {
-        this.generateMembers()
-      },
+export default {
+  name: 'GameCanvas',
+  data () {
+    return {
+      x: 0,
+      y: 0,
+      result: '?',
+      from: -100,
+      to: 100,
+      expectedResult: 0,
+      numberOfButtons: 3,
+      answerOptions: []
     }
+  },
+  methods: {
+    generateNumber(from, to) {
+      return Math.ceil(Math.random()*(to - from) + from)
+    },
+    generateMembers(){
+      this.x = this.generateNumber(this.from, this.to);
+      this.y = this.generateNumber(this.from, this.to);
+      this.expectedResult = this.x + this.y
+      this.answerOptions.push(this.expectedResult)
+      for (let i=0; i < this.numberOfButtons - 1; i++) {
+        const newOption = this.expectedResult + this.generateNumber(-10, 10);
+        this.answerOptions.push(newOption)
+      };
+      this.shuffle(this.answerOptions)
+    },
+    shuffle(targetArray) {
+      return targetArray.sort(() => {
+        return Math.random() - 0.5;
+      })
+    },
+    insertOption(event) {
+      this.result = event.target.innerHTML;
+    },
+    proceed() {
+      if (this.result === '?') {
+        alert('Please select the answer')
+        return;
+      }
+      if (this.result == this.expectedResult) {
+        this.$emit('checkOneAnswer', 'Your answer is correct !')
+      } else {
+        this.$emit('checkOneAnswer', `Your answer is wrong,
+        Correct answer is ${this.expectedResult}!
+        Would you like to proceed`)
+      }
+      this.$emit('switchView', 'Info')
+    }
+  },
+  created () {
+    this.generateMembers()
+  }
+}
 </script>
 
 <style scoped>
-  .greeting {
+.greeting {
     width: 70%;
     margin: 20px auto;
     text-align: center;
-  }
+}
 
-  .answers {
-    margin: 12px;
-    width: 70px;
-  }
+.answers {
+  margin: 12px;
+  width: 70px;
+}
 </style>
